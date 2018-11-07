03627e60673409e38772912b2f091d052c543a9e
Rajnish <rajnish.kucheria@gmail.com>
2018-11-07 16:44:19 +0530

    dev: add db crud operation

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var http = require('http');
 var express = require('express');
 var fs = require('fs');
 var app = express();
-
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 var bodyParser = require('body-parser');
+var shortnerService = require('./shortner.service');
+var ourDomainUrl = 'localhost:5000/';
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,35 +15,97 @@ app.use(bodyParser.json());
 
 app.set('view engine','ejs');
 
-function add_link(link, id){
-  console.log('add_link - add short url and actual url to db');
-}
 
-function get_link(uid){
- console.log('get_link - add short url and actual url to db'); 
+
+console.log('Working now!');
+var url = 'mongodb://localhost:27017/database';
+// var url ='mongodb://myinapp:myinapp@ds119449.mlab.com:19449/myinapp'
+
+var link_db;
+
+
+/*get_link : function to get the link 
+  from the database.
+  
+params:
+  uid - unique id for every link 
+  that was generated before.
+  request - request object
+  resp - response object
+  
+*/
+function get_link(uid,request,resp){
+
+  var id_db = uid;
+  MongoClient.connect(url,function(err,db){
+    if(err){
+      console.log(err);
+    }
+    else{
+      var collection = db.collection('uids');
+
+      collection.find({id:id_db}).toArray(function(err,res){
+        if(err){
+          console.log(err);
+        }
+        else if(res.length == 1){
+          link_db = res[0]['link'];
+          resp.writeHead(302, {
+            'Location': link_db
+            //add other headers here...
+          });
+          resp.end();
+        }
+        else{
+          console.log('No such UID exists!');
+          resp.writeHead(301,
+          {Location: '/short/error404'}
+          );
+          link_db = 'NULL';
+          resp.end();
+        }
+        db.close();
+      });
+
+    }
+
+  });
 }
-app.get('/',function(){
+app.get('/',function(req,res){
   console.log('Shortner Redirector');
+  res.writeHead(301,
+  {Location: 'http://localhost:5000/short'}
+  );
+  res.end();
 });
 
-app.get('/short',function(){
-  console.log('main page for shorten url');
+app.get('/short',function(req,res){
+  res.writeHead(200,{'Content-Type':'text/html'});
+  var myInputStream = fs.createReadStream(__dirname + "/shortner.html");
+  myInputStream.pipe(res);
 });
 
 
-app.post('/short/url',function(){
-  // generates unique ID redirect to the url.
-  console.log();
+app.post('/short/url',function(req,res){
+  var uid = shortnerService.getUid();
+  shortnerService.addToDb(ourDomainUrl, req.body.mainurl, uid);
+  console.log('/short/url----------------',uid)
+  res.render('uid',{uniqueid: ourDomainUrl + uid});
+  res.end(ourDomainUrl + uid);
 });
 
 
 app.get('/:uid',function(req,res){
-    console.log('get the given id');
+  get_link("localhost:5000/" + req.params.uid,req,res);
+  console.log('Redirector');
 });
 
 
 app.get('/short/error404',function(req,res){
   console.log('Error Page!');
+  res.writeHead(200,{'Content-Type':'text/html'});
+  var myInputStream = fs.createReadStream(__dirname + "/error.html");
+  myInputStream.pipe(res);
 });
 
 
--- shortner.service.js
+++ shortner.service.js
@@ -1,6 +1,10 @@
 function ShortnerService () {
   var self = this;
   self.shortid = require('shortid');
+  self.mongodb = require('mongodb');
+  self.MongoClient = self.mongodb.MongoClient;
+  self.url = 'mongodb://localhost:27017/database';
+
 }
 
 function getUidObject(ourDomainUrl, externalurl, id) {
@@ -16,10 +20,77 @@ function getUid () {
   return self.shortid.generate();
 }
 
+function addToDb(ourDomainUrl, externalUrl, uid){
+  var self = this;
+
+  self.MongoClient.connect(self.url, function(err, db){
+    if (err){
+      console.log(err);
+    }
+    else {
+      var collection = db.collection('uids');
+      var obj = shortnerService.getUidObject(ourDomainUrl, externalUrl, uid);
+      
+      collection.insert(obj,function(err,res){
+
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log('%d Links Entered!', res.insertedCount);
+        }
+        db.close();
+      });
+
+      }
+  });
+}
+
+function getFromDb(uid, request, resp){
+  var self = this;
+
+  var id_db = uid;
+  self.MongoClient.connect(self.url, function(err, db){
+    if(err){
+      console.log(err);
+    }
+    else{
+      var collection = db.collection('uids');
+
+      collection.find({id:id_db}).toArray(function(err,res){
+        console.log('length---------------:', res.length);
+        if(err){
+          console.log(err);
+        }
+        else if(res.length == 1){
+          link_db = res[0]['link'];
+          resp.writeHead(302, {
+            'Location': link_db
+            //add other headers here...
+          });
+          resp.end();
+        }
+        // else{
+        //   console.log('No such UID exists!');
+        //   resp.writeHead(301,
+        //   {Location: '/short/error404'}
+        //   );
+        //   link_db = 'NULL';
+        //   resp.end();
+        // }
+        db.close();
+      });
+
+    }
+
+  });
+}
 
 ShortnerService.prototype = {
   getUidObject: getUidObject,
-  getUid: getUid
+  getUid: getUid,
+  addToDb: addToDb,
+  getFromDb: getFromDb
 };  
 
 var shortnerService = new ShortnerService();
